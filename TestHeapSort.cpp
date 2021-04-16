628747d7addb95353cbe61c5829d5b895a3fb834
Victor <biliavskyi.viktor@lll.kpi.ua>
2021-04-16 20:46:04 +0300

    Completed

--- TestHeapSort.cpp
+++ TestHeapSort.cpp
@@ -80,10 +80,73 @@ void testHeapSortSpeed() {
     clock_t timeEnd4 = clock();
     float time4 = (float(timeEnd4 - timeStart4)) / CLOCKS_PER_SEC;
 
+    clock_t timeStart5 = clock();
+        heapSort(temp, capacity);
+    clock_t timeEnd5 = clock();
+    float time5 = (float(timeEnd5 - timeStart5)) / CLOCKS_PER_SEC;
+
+    clock_t timeStart6 = clock();
+        make_heap(temp, temp+capacity);
+        sort_heap(temp, temp+capacity);
+    clock_t timeEnd6 = clock();
+    float time6 = (float(timeEnd6 - timeStart6)) / CLOCKS_PER_SEC;
+
+    clock_t timeStart7 = clock();
+        bubbleSort(temp, capacity);
+    clock_t timeEnd7 = clock();
+    float time7 = (float(timeEnd7 - timeStart7)) / CLOCKS_PER_SEC;
+
+    clock_t timeStart8 = clock();
+        insertionSort(temp, capacity);
+    clock_t timeEnd8 = clock();
+    float time8 = (float(timeEnd8 - timeStart8)) / CLOCKS_PER_SEC;
+
+    reverse(temp, temp+capacity);
+
+    clock_t timeStart9 = clock();
+        heapSort(temp, capacity);
+    clock_t timeEnd9 = clock();
+    float time9 = (float(timeEnd9 - timeStart9)) / CLOCKS_PER_SEC;
+
+    reverse(temp, temp+capacity);
+
+    clock_t timeStart10 = clock();
+        make_heap(temp, temp+capacity);
+        sort_heap(temp, temp+capacity);
+    clock_t timeEnd10 = clock();
+    float time10 = (float(timeEnd10 - timeStart10)) / CLOCKS_PER_SEC;
+
+    reverse(temp, temp+capacity);
+
+    clock_t timeStart11 = clock();
+        bubbleSort(temp, capacity);
+    clock_t timeEnd11 = clock();
+    float time11 = (float(timeEnd11 - timeStart11)) / CLOCKS_PER_SEC;
+
+    reverse(temp, temp+capacity);
+
+    clock_t timeStart12 = clock();
+        insertionSort(temp, capacity);
+    clock_t timeEnd12 = clock();
+    float time12 = (float(timeEnd12 - timeStart12)) / CLOCKS_PER_SEC;
+
+    cout << "Sort times on randomly unsorted array:" << endl;
     cout << setw(25) << left << "Time of my heap sort: " << setw(6) << right << time1 << endl;
     cout << setw(25) << left << "Time of STL heap sort: " << setw(6) << right << time2 << endl;
     cout << setw(25) << left << "Time of bubble sort: " << setw(6) << right << time3 << endl;
-    cout << setw(25) << left << "Time of insertion sort: " << setw(6) << right << time4 << endl;
+    cout << setw(25) << left << "Time of insertion sort: " << setw(6) << right << time4 << endl << endl;
+
+    cout << "Sort times on sorted array:" << endl;
+    cout << setw(25) << left << "Time of my heap sort: " << setw(6) << right << time5 << endl;
+    cout << setw(25) << left << "Time of STL heap sort: " << setw(6) << right << time6 << endl;
+    cout << setw(25) << left << "Time of bubble sort: " << setw(6) << right << time7 << endl;
+    cout << setw(25) << left << "Time of insertion sort: " << setw(6) << right << time8 << endl << endl;
+
+    cout << "Sort times on completely unsorted array:" << endl;
+    cout << setw(25) << left << "Time of my heap sort: " << setw(6) << right << time9 << endl;
+    cout << setw(25) << left << "Time of STL heap sort: " << setw(6) << right << time10 << endl;
+    cout << setw(25) << left << "Time of bubble sort: " << setw(6) << right << time11 << endl;
+    cout << setw(25) << left << "Time of insertion sort: " << setw(6) << right << time12 << endl << endl;
 
     delete [] array;
     delete [] temp;
@@ -108,7 +171,7 @@ bool testHeapSort() {
     testHeapSortSpeed();
     if (isDataEqual)
     {
-        cout << endl << "The sort is working fine" << endl << endl;
+        cout << "The sort is working fine" << endl << endl;
         return true;
     }
     cerr << "Sort doesn't work properly" << endl << endl;
